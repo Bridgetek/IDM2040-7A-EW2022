d92ed5d77f61b1f44dce2446c270d4b9be1478be
qingfeng <qingfeng.zhang@brtchip.com>
2022-07-01 09:37:36 +0800

    Update code.py

--- LDS_Demo/code.py
+++ LDS_Demo/code.py
@@ -1,12 +1,11 @@
 from brteve.brt_eve_bt817_8 import BrtEve
 from brteve.brt_eve_rp2040 import BrtEveRP2040
-import ui_lds as ui_lds
+from ui_lds.ui_main import ui_main
 
 host = BrtEveRP2040()
 eve = BrtEve(host)
-eve.init(resolution="800x480", touch="capacity")
- 
-#ui_lds.ui_main(eve,True)    # skip Sensor
-ui_lds.ui_main(eve,False)  # use LDS Sensor
+eve.init(resolution="800x480", touch="capacity") 
+#ui_main(eve,True)    # skip Sensor
+ui_main(eve,False)  # use LDS Sensor
 
  
